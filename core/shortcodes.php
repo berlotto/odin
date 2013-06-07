1f5a61e5b857b71fc20437020c7873828253aea1
claudiosmweb <contato@claudiosmweb.com>
2013-06-07 02:09:06 -0300

    added alert shortcode #53

--- core/shortcodes.php
+++ core/shortcodes.php
@@ -28,3 +28,25 @@ function odin_shortcode_button( $atts, $content = null ) {
 }
 
 add_shortcode( 'button', 'odin_shortcode_button' );
+
+/**
+ * Alert shortcode.
+ *
+ * @param  array  $atts    Attributes.
+ * @param  string $content Content.
+ *
+ * @return string          Alert HTML.
+ */
+function odin_shortcode_alert( $atts, $content = null ) {
+    extract( shortcode_atts( array(
+        'class' => ''
+    ), $atts ) );
+
+    $html = '<div class="alert ' . $class . '"><button type="button" class="close" data-dismiss="alert">&times;</button>';
+    $html .= $content;
+    $html .= '</div>';
+
+    return $html;
+}
+
+add_shortcode( 'alert', 'odin_shortcode_alert' );
